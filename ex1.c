74e7e9a6f811e3cdeac0fa70b2f8b9ba550b2885
Rashmika2590 <107987098+Rashmika2590@users.noreply.github.com>
2023-08-03 13:01:35 -0400

    Add files via upload

---
+++ ex1.c
@@ -0,0 +1,14 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main(){
+#pragma omp parallel
+    {
+        int nt = omp_get_num_threads();
+        int id = omp_get_thread_num();
+        printf("\n I am thread %d of %d",id,nt);
+
+    }
+    return 0;
+}
+
